6ae81183e612e98983b9830f5be4a7d99819a619
Kito Cheng <kito.cheng@sifive.com>
2021-04-29 13:54:35 +0800

    RISC-V: Fix open syscall for libgloss on qemu
    There is two issue which made open syscall not work with qemu:
    1. RISC-V qemu don't provide `open` syscall, only `openat`, so we
       redirect open to openat with AT_FDCWD.
    2. Newlib using different value for AT_FDCWD and flags with linux,
       we need to translate flags and dirfd to linux style.

--- libgloss/riscv/sys_open.c
+++ libgloss/riscv/sys_open.c
@@ -1,9 +1,19 @@
 #include <machine/syscall.h>
 #include "internal_syscall.h"
 
+#ifdef USING_QEMU
+#include <fcntl.h>
+
+extern int _openat(int dirfd, const char *name, int flags, int mode);
+#endif
+
 /* Open a file.  */
 int
 _open(const char *name, int flags, int mode)
 {
+#ifdef USING_QEMU
+  return _openat (AT_FDCWD, name, flags, mode);
+#else
   return syscall_errno (SYS_open, 3, name, flags, mode, 0, 0, 0);
+#endif
 }
--- libgloss/riscv/sys_openat.c
+++ libgloss/riscv/sys_openat.c
@@ -1,8 +1,47 @@
 #include <machine/syscall.h>
 #include "internal_syscall.h"
 
+#ifdef USING_QEMU
+#include <fcntl.h>
+
+/* Newlib and linux using differnt value for AT_FDCWD.  */
+#define LINUX_AT_FDCWD           -100
+
+/* And also using differnt value for open's flags.  */
+#define LINUX_O_CREAT 0100
+#define LINUX_O_TRUNC 01000
+#define LINUX_O_APPEND 02000
+
+static int traslate_flags(int flags)
+{
+  /* O_WRONLY and O_RDWR are same as Linux.  */
+  int new_flags = flags & (O_RDWR | O_WRONLY);
+
+  if (flags & O_CREAT)
+    new_flags |= LINUX_O_CREAT;
+
+  if (flags & O_TRUNC)
+    new_flags |= LINUX_O_TRUNC;
+
+  if (flags & O_APPEND)
+    new_flags |= LINUX_O_APPEND;
+
+  return new_flags;
+}
+#endif
+
 /* Open file relative to given directory.  */
 int _openat(int dirfd, const char *name, int flags, int mode)
 {
-  return syscall_errno (SYS_openat, 4, dirfd, name, flags, mode, 0, 0);
+  int traslated_flags_dirfd = dirfd;
+  int traslated_flags = flags;
+
+#ifdef USING_QEMU
+  traslated_flags = traslate_flags (flags);
+  if (dirfd == AT_FDCWD)
+    traslated_flags_dirfd = LINUX_AT_FDCWD;
+#endif
+
+  return syscall_errno (SYS_openat, 4, traslated_flags_dirfd,
+			name, traslated_flags, mode, 0, 0);
 }
